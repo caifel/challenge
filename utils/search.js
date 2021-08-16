cc26cf8a09114ca49864c8fdc547f555e7bd6da6
Mario Medrano (Contractor) <mmedrano@m-c02dj1qnml85.paloaltonetworks.local>
2021-08-16 11:06:57 -0700

    challenge: Imprve search fn

--- utils/search.js
+++ utils/search.js
@@ -1,12 +1,21 @@
 export const search = (list, searchInput = "") => {
-	const regex = new RegExp(searchInput, "i");
+	const isRegex =
+		searchInput[0] === "/" && searchInput[searchInput.length - 1] === "/";
+	const regex = new RegExp(searchInput.slice(1, -1), "i");
 
 	if (searchInput) {
-		return list.filter(
-			({ name, code }) =>
-				regex.test(name?.toLowerCase()) ||
-				regex.test(code?.toLowerCase())
-		);
+		return list.filter(({ name, code }) => {
+			if (isRegex) {
+				return (
+					regex.test(name?.toLowerCase()) ||
+					regex.test(code?.toLowerCase())
+				);
+			}
+			return (
+				name?.toLowerCase().startsWith(searchInput.toLowerCase()) ||
+				code?.toLowerCase().startsWith(searchInput.toLowerCase())
+			);
+		});
 	}
 	return [];
 };
